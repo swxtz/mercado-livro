5149a30795abbdf0a01902d1503b50a0280dfef1
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-02 22:57:19 -0300

    feat: finding customeer by id

--- src/main/kotlin/com/mercadolivro/controller/CustomeerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomeerController.kt
@@ -4,11 +4,13 @@ import com.mercadolivro.controller.request.PostCustomeer
 import com.mercadolivro.model.CustomeerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpStatusCodeException
 
 @RestController
 @RequestMapping("/customeers")
@@ -16,7 +18,7 @@ class CustomeerController {
 
     val customeers = mutableListOf<CustomeerModel>()
     @GetMapping
-    fun getCustomeer(): MutableList<CustomeerModel> {
+    fun getAll(): MutableList<CustomeerModel> {
         return customeers
     }
 
@@ -24,6 +26,11 @@ class CustomeerController {
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customeer: PostCustomeer) {
         customeers.add(CustomeerModel(customeers.size + 1, customeer.name, customeer.email))
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomeer(@PathVariable id: Int): CustomeerModel {
+            return customeers.filter { it.id == id }.first()
     }
 
 }
