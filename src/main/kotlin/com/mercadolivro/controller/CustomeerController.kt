57ccfa173caeec9a9ad0e8d253b1082c3158c6af
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-02 22:18:25 -0300

    feat: add status code

--- src/main/kotlin/com/mercadolivro/controller/CustomeerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomeerController.kt
@@ -1,10 +1,13 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomeer
 import com.mercadolivro.model.CustomeerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,8 +19,9 @@ class CustomeerController {
     }
 
     @PostMapping
-    fun create(@RequestBody customeer: CustomeerModel): CustomeerModel {
-        return customeer
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customeer: PostCustomeer) {
+        println(customeer)
     }
 
 }
---
+++ src/main/kotlin/com/mercadolivro/controller/request/PostCustomeer.kt
@@ -0,0 +1,6 @@
+package com.mercadolivro.controller.request
+
+data class PostCustomeer (
+    var name: String,
+    var email: String,
+)
